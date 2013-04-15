7ed41060566cbd31bf97bf6bd2e8f4f72646b580
Bruno Abrantes <inf0rmer.realm@gmail.com>
2013-04-15 11:04:00 +0100

    change template extension

--- grunt.js
+++ grunt.js
@@ -81,7 +81,7 @@ module.exports = function(grunt) {
       app: {
         src: ['dist/backbone-validation.js'],
         options: {
-          template: 'docco.jst'
+          template: 'docco.tpl'
         }
       }
     },
